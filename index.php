5ecfd87fe1d3f43901dcb1fa57c38fbe721b6571
Ivan8505 <Ivan2g.rodriigues@gmail.com>
2023-10-07 12:34:50 -0300

    2 - Ajustes

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Meteor-Ia</title>
+        <link rel="stylesheet" href="src/style.css">
+    </head>
+    <body>
+
+      <main>
+        <div class="row">
+        <div class="col">
+                test1
+            </div><div class="col">
+                test1
+            </div>
+        </div>
+      </main>
+
+      <script src="src/script.js"></script>
+    </body>
+</html>
