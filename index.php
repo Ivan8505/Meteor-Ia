3f87ce34f46047467581ad999c0a1163d01bbc8c
Ivan8505 <Ivan2g.rodriigues@gmail.com>
2023-10-07 16:01:37 -0300

    3 - Layout

--- index.php
+++ index.php
@@ -9,15 +9,82 @@
     <body>
 
       <main>
-        <div class="row">
-        <div class="col">
-                test1
-            </div><div class="col">
-                test1
+        <nav class="navbar fixed-top navbar-expand-lg menu">
+          <div class="container">
+                
+            <div class="navbar-brand">
+              <a href="#">
+                <img src="assets/nasa.png" width="64" height="64" alt="">
+              </a>
             </div>
+
+            <div class="d-flex justify-content-center align-items-center navbar-brand" role="search">
+                <input class="form-control me-2 text-center" id="input" type="search" placeholder="Search" aria-label="Search">
+                <a href="#">
+                  <img src="assets/nasa.png" width="40" height="40" alt="">
+                </a>
+            </div>
+
+            <div class="navbar-brand">
+              <a href="#">
+                <img src="assets/nasa.png" width="64" height="64" alt="">
+              </a>
+            </div>
+
+            <div class="navbar-brand links-menu">
+              <a href="#">Login</a>
+              <a href="#">Cadastro</a>
+            </div>
+
+            </div>  
+        </nav>
+        <div class="container">
+
+          <div class="title">
+            <h3 class="title-h3">Meteor-IA</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quas deleniti tempore. Fugit deleniti qui repudiandae, odio eos quidem voluptatem ducimus ab voluptatum officia nesciunt veritatis. Enim ullam saepe ea.</p>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <h4>Lorem</h4>
+
+              <div class="d-flex justify-content-between left flex-column" >
+                
+                <div>
+                  <div>
+                    <span>Tarde:</span>
+                    <img src="assets/nasa.png" width="32" height="32" alt=""> 
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <span>Noite:</span>
+                    <img src="assets/nasa.png" width="32" height="32" alt=""> 
+                  </div>
+                </div>
+
+            </div>
+              
+            </div>
+            
+            <div class="col-md-6">
+              <h4>Lorem</h4>
+
+              <div class="d-flex justify-content-between right">
+
+                
+
+              </div>
+            </div>
+
+          </div>
+
         </div>
+
       </main>
 
-      <script src="src/script.js"></script>
+    
     </body>
 </html>
